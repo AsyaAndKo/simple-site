2a997ddede3faa25607594f9c7c1f0848e387030
AsyaAndKo <asya.kosareva666@gmail.com>
2022-03-09 15:58:18 +0200

    Add search form to navbar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Container, Nav, Navbar, Form, Button } from "react-bootstrap";
 import logo from "../assets/kindpng_1290643.png";
 
 export default class Header extends Component {
   render() {
     return (
-      <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+      <Navbar collapseOnSelect expand="md" bg="blue" variant="blue">
         <Container>
           {/* Create navbar with logo aligned */}
           <Navbar.Brand href="/">
@@ -22,12 +22,16 @@ export default class Header extends Component {
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             {/* Merge right automatically */}
-            <Nav className="mr-auto">
+            <Nav className="me-auto">
               <Nav.Link href="/">Home</Nav.Link>
               <Nav.Link href="/about">About</Nav.Link>
               <Nav.Link href="/contacts">Contacts</Nav.Link>
               <Nav.Link href="/blog">Blog</Nav.Link>
             </Nav>
+            <Form className="d-flex">
+              <Form.Control type="text" placeholder="Search" className="me-2" />
+              <Button variant="outline-info">Search</Button>
+            </Form>
           </Navbar.Collapse>
         </Container>
       </Navbar>
